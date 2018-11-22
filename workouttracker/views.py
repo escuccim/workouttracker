57666d83928dfc84160c4208c346e72f1c1744cd
Eric Scuccimarra <skooch@gmail.com>
2018-11-22 11:38:34 +0100

    FIxing redirect problem

--- workouttracker/views.py
+++ workouttracker/views.py
@@ -45,7 +45,7 @@ def fig_to_base64(fig):
 # Create your views here.
 def Index(request):
     if not request.user.is_authenticated:
-        return redirect('%s?next=%s' % ('register:login', request.path))
+        return redirect('%s?next=%s' % ('/en/user/login', request.path))
 
     user = request.user
     start_date, end_date = get_dates_from_request(request)
